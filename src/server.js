0c2c1e4de75389aadbdb59dbfc2787b54c7ed63e
Student <student@example.com>
2024-12-04 20:30:34 +0700

    vercel

--- src/server.js
+++ src/server.js
@@ -22,5 +22,5 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 });
